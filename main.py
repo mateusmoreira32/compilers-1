0dc29820ce0113359466af89209f41aabbd17b31
mateusmoreira32 <74328621+mateusmoreira32@users.noreply.github.com>
2021-08-24 21:24:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,46 @@
-email = 'guihss.cs@gmail.com'
+import unittest
+from main import toInt
 
-
-# Exemplo
-def verify_email(email):
-    prefix = True
-    domain = False
-
-    for c in list(email):
-
-        if prefix:
-
-            if c.isalnum() or c == '.':
-                continue
-            elif c == '@':
-                prefix = False
-                domain = True
-                continue
-            else:
-                print(c, 'is invalid')
-                return False
-
-        elif domain:
-
-            if c.isalnum() or c == '.':
-                continue
-
-            else:
-                print(c, 'is invalid')
-                return False
-
-
-    if prefix:
-        return False
-
-    return True
-
-
-def verify_date(date):
-
-    pass
-
-
-
-def toInt(number):
-
-    pass
-
+class TesteVerificarToInt(unittest.TestCase):
+    def teste_correto(self):
+        self.assertEqual(toInt('1243') + 1,1244)
+        self.assertEqual(toInt('311') + 5 ,316)
+        self.assertEqual(toInt('10') -1, 9)
+ class TesteVerificarData(unittest.TestCase):
+    def test_invalid_dates(self):
+        DatasInvalidas = [
+            'qwcascasc'
+            '11/ca/1999']
+        for DatasInvalidas in DatasInvalidas:
+            self.assertFalse(verificar_data(DatasInvalidas))
+    def teste_validar(self):
+        validarDatas = [
+            '12/11/10'
+            '11/10/21'
+            '12/06/13']
+        for validarDatas in validarDatas:
+            self.assertTrue(verificar_data(DatasInvalidas))
+  class test_invalid_emails(self):
+    def test_invalid_emails(self):
+        invalidarEmails = [
+            'mat-@mail.com',
+            'mat..@mail.com',
+            '.mat@mail.com',
+            'mat#@mail.com',
+            'mat.def@mail.com',
+            'mat.def@mail.c',
+            'mat.def@mail#archive.com'
+        ]
+        for invalidarEmail in invalidarEmails:
+            self.assertFalse(verificar_email(invalidarEmail))
+    def test_validar_emails(self):
+        validarEmails = [
+            'mat-d@mail.com',
+            'mat.def@mail.com',
+            'mat@mail.com',
+            'mat_def@mail.com',
+            'mat.def@mail.com',
+        ]
+        for validarEmail in validarEmails:
+            self.asserttrue(verificar_email(validarEmail))
+            
